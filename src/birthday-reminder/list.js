3b1ccbb99627bc5275bfa25cd3ce3098b14d2bbd
Duy Anh <huynhanh.mtu@gmail.com>
2022-01-07 21:54:18 +0700

    Update

--- src/birthday-reminder/list.js
+++ src/birthday-reminder/list.js
@@ -1,7 +1,7 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import ListItem from './listItem';
 
-export default class ListReminder extends Component {
+export default class ListReminder extends PureComponent {
   handleRenderList = () => {
     return this.props.listReminder.map((data) => {
       return <ListItem key={data.id} data={data} />;
