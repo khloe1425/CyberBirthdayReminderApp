516263d206feeb6ca51b35f7c4fb548c899967a6
Duy Anh <huynhanh.mtu@gmail.com>
2022-01-07 13:17:14 +0700

    Update

--- src/birthday-reminder/index.js
+++ src/birthday-reminder/index.js
@@ -18,11 +18,14 @@ export default class Reminder extends Component {
   }
 
   componentDidMount() {
-    setInterval(() => {
+    this.timerID = setInterval(() => {
       this.setState({
         date: new Date()
       });
     }, 1000);
+  }
+  componentWillUnmount() {
+    clearInterval(this.timerID);
   }
 
   render() {
